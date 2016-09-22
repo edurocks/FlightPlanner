cc09b2b5152020a32234dcc6a72064c2f5f55ce2
Eduardo <joseeduardo.cardosodacosta.ext@altran.com>
2016-09-22 21:02:48 +0100

    Including base libraries Configure Stetho for debug

---
+++ app/src/main/java/dmanlancers/com/flightplanner/application/FlightPlannerApplication.java
@@ -0,0 +1,21 @@
+package dmanlancers.com.flightplanner.application;
+
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
+public class FlightPlannerApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                        .build());
+    }
+}
