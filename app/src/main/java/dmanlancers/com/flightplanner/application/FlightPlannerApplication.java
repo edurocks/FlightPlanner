d8bc2651b0aad448146a8d0e2134d676d0c35eaf
Eduardo <joseeduardo.cardosodacosta.ext@altran.com>
2016-09-24 22:10:38 +0100

    Default configuration for realm database

--- app/src/main/java/dmanlancers/com/flightplanner/application/FlightPlannerApplication.java
+++ app/src/main/java/dmanlancers/com/flightplanner/application/FlightPlannerApplication.java
@@ -6,16 +6,33 @@ import android.app.Application;
 import com.facebook.stetho.Stetho;
 import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class FlightPlannerApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        initStetho();
+        initRealm();
+    }
 
+    private void initStetho() {
         Stetho.initialize(
                 Stetho.newInitializerBuilder(this)
                         .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
                         .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
                         .build());
+    }
+
+    private void initRealm() {
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder(this)
+                .name(Realm.DEFAULT_REALM_NAME)
+                .schemaVersion(0)
+                .deleteRealmIfMigrationNeeded()
+                .build();
+
+        Realm.setDefaultConfiguration(realmConfiguration);
     }
 }
