88324a9bbc1d1e7c27d85c6242b2857696a036b9
firestruck <celso.r.n.rodrigues@gmail.com>
2016-10-05 19:29:41 +0100

    Splash screen added

---
+++ app/src/main/java/dmanlancers/com/flightplanner/activities/SplashActivity.java
@@ -0,0 +1,36 @@
+package dmanlancers.com.flightplanner.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import dmanlancers.com.flightplanner.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static boolean splashLoaded = false;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (!splashLoaded) {
+            setContentView(R.layout.splash_activity);
+            int secondsDelayed = 6;
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                    finish();
+                }
+            }, secondsDelayed * 500);
+
+            splashLoaded = true;
+        } else {
+            Intent goToMainActivity = new Intent(SplashActivity.this, LoginActivity.class);
+            goToMainActivity.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(goToMainActivity);
+            finish();
+        }
+    }
+}
